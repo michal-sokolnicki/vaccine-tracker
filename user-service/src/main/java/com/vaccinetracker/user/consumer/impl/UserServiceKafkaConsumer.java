b303f874a924bd92edac09d516dd84895b778df0
micsokol2 <michal.sokolnicki@publicissapient.com>
2022-04-14 16:06:30 +0200

    "config improvements and consumer added"

---
+++ user-service/src/main/java/com/vaccinetracker/user/consumer/impl/UserServiceKafkaConsumer.java
@@ -0,0 +1,56 @@
+package com.vaccinetracker.user.consumer.impl;
+
+import com.vaccinetracker.config.KafkaConfigData;
+import com.vaccinetracker.config.KafkaConsumerConfigData;
+import com.vaccinetracker.kafka.admin.client.KafkaAdminClient;
+import com.vaccinetracker.kafka.avro.model.BookingAvroModel;
+import com.vaccinetracker.user.consumer.KafkaConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class UserServiceKafkaConsumer implements KafkaConsumer<Long, BookingAvroModel> {
+
+    private final KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+    private final KafkaAdminClient kafkaAdminClient;
+    private final KafkaConfigData kafkaConfigData;
+    private final KafkaConsumerConfigData kafkaConsumerConfigData;
+
+    public UserServiceKafkaConsumer(KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry,
+                                    KafkaAdminClient kafkaAdminClient, KafkaConfigData kafkaConfigData, KafkaConsumerConfigData kafkaConsumerConfigData) {
+        this.kafkaListenerEndpointRegistry = kafkaListenerEndpointRegistry;
+        this.kafkaAdminClient = kafkaAdminClient;
+        this.kafkaConfigData = kafkaConfigData;
+        this.kafkaConsumerConfigData = kafkaConsumerConfigData;
+    }
+
+    @EventListener
+    public void onAppStarted(ApplicationStartedEvent event){
+        kafkaAdminClient.checkTopicsCreated();
+        log.info("Topics with name {} is ready for operations!", kafkaConfigData.getTopicNamesToCreate().toArray());
+        Objects.requireNonNull(kafkaListenerEndpointRegistry.getListenerContainer(kafkaConsumerConfigData.getConsumerGroupId())).start();
+    }
+
+    @Override
+    @KafkaListener(id = "${kafka-consumer-config.consumer-group-id}", topics = "${kafka-config.topic-name}")
+    public void receive(@Payload List<BookingAvroModel> messages,
+                        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) List<Integer> keys,
+                        @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
+                        @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+        log.info("Kafka listener has been woke up and has received {} number of message with keys {}, " +
+                        "partitions {} and offsets {} and will be send to elastic: Thread id {}",
+                messages.size(), keys, partitions.toString(),offsets.toString(), Thread.currentThread().getId());
+
+    }
+}
