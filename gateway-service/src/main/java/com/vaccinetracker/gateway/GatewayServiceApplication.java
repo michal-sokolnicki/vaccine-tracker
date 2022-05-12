48f50b9a649cb5ed129cb88ca2260b8a47803f86
micsokol2 <michal.sokolnicki@publicissapient.com>
2022-05-12 16:32:09 +0200

    "api gateway added"

--- gateway-service/src/main/java/com/vaccinetracker/gateway/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/vaccinetracker/gateway/GatewayServiceApplication.java
@@ -2,10 +2,10 @@ package com.vaccinetracker.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan(basePackages = "com.vaccinetracker")
 public class GatewayServiceApplication {
